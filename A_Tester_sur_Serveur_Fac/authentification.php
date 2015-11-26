0a8a8990e5dc3dd707a5620b69d9cd97f9a298b7
U-DESKTOP-HB796GP\ccbal <ccbalexandretabot@orange.fr>
2015-11-26 03:11:15 +0100

    correction categories.php , categories2.php, authentification.php sur WAMP local

---
+++ A_Tester_sur_Serveur_Fac/authentification.php
@@ -0,0 +1,26 @@
+<head>
+		<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="style1ELudotheque.css" media="screen" />
+		
+		<title> Catalogue E-ludothèque </title>
+	</head>
+<?php
+		
+		//paramètres de connexion à la base de données $Base="nom-base";
+		$Serveur= "info.univ-lemans.fr";
+		$Utilisateur="info201a_user";
+		$MotDePasse="com72";
+		$Base = "info201a";
+		//connexion au serveur où se trouve la base de données
+  		$LienBase= mysql_connect($Serveur,$Utilisateur,$MotDePasse);
+  		//sélection de la base de données au niveau du serveur
+  		$retour=mysql_select_db($Base,$LienBase);
+  		
+  		//affichage d’un message d’erreur si la connexion a été refusée
+  		if(!$retour) {
+    		echo "\nConnexion a la base impossible";
+		}
+		else {
+		    	echo "\n Connexion a la base reussie";
+		}
+?>
---
+++ A_Tester_sur_Serveur_Fac/categorie2.php
@@ -0,0 +1,79 @@
+﻿<head>
+
+	<link rel="stylesheet" type="text/css" href="style1ELudotheque.css" media="screen" />
+
+	<title> Présentation Eludothèque </title>
+	
+</head>
+	
+<body>
+	
+	<! Volet de navigation du site>
+ 
+ 	<ul class="volet">
+
+		<th>
+
+			<a href="./page2.html"> Présentation</a>
+
+		</th>
+
+		<th>
+
+			<a href="./pageCatalogue.html">Le Catalogue</a>
+
+		</th>
+
+		<th>
+
+			<a href="./pageInfosPratiques.html"> Informations Pratiques - Contact</a>
+
+		</th>
+
+		<th>
+
+			<a href="./pageAccesPanierCompte.html"><img src="./ressourcesImage/panier_Eludo.jpg"></a>
+
+		</th>
+
+	</ul>
+	
+</body>
+
+<?php
+	
+	//paramètres de connexion à la base de données $Base="nom-base";
+
+	$Serveur= "info.univ-lemans.fr";
+	$Utilisateur="info201a_user";
+	$MotDePasse="com72";
+
+	$Base = "info201a";
+
+	$LienBase= mysql_connect($Serveur,$Utilisateur,$MotDePasse);
+	$retour=mysql_select_db($Base,$LienBase);
+	//Ecriture de la requête en SQL (voir TP)
+
+	$Requete="SELECT * FROM FC_grp5_Jeux;";
+
+	//Envoi de la requête
+	$Reponse = mysql_query($Requete);
+	// traitement
+  	echo "<table border=1>";
+
+
+	//Traitement ligne par ligne de la réponse
+
+	while ($donnees = mysql_fetch_array($Reponse) )
+ {
+
+		//Affichage des lignes de données, champ par champ
+
+		echo "Nom du jeux: ".$donnees[0]." Age requis: " .$donnees[1]."  " .$donnees[2]."<br></br>";
+
+	}
+
+	mysql_close();
+
+?>
+
